29ec8aa1efa5ec1746ad55b0f6a51e641e3e6f59
Official-Rahul <official.rahul.email@gmail.com>
2021-05-13 19:18:06 +0530

    addition logic done

--- js/cal.js
+++ js/cal.js
@@ -1,5 +1,23 @@
-var num1, num2;
+var num1, num2, opr;
 function buttonClick(num){
     var oldValue = document.getElementById('display').value;
+    if(oldValue=='+'){
+        oldValue='';
+    }
     document.getElementById('display').value = oldValue+num;
+}
+
+function operatorClick(operator) {
+    num1 = document.getElementById('display').value;
+    document.getElementById('display').value=operator;
+    opr = operator;
+}
+
+function equalClick(){
+    num2=document.getElementById('display').value;
+    num1=parseInt(num1);
+    num2=parseInt(num2);
+    if(opr=='+') {
+        document.getElementById('display').value=num1+num2;
+    }
 }
